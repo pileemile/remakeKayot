6c00152b7e13334a9d57a0241a7f921c14b0fb8a
emile <emilepicard323@gmail.com>
2025-08-08 09:59:24 +0200

    ajustement question answers

---
+++ src/app/models/attempts/attempts.ts
@@ -0,0 +1,7 @@
+export interface Attempts {
+  id: string
+  quiz_id: string | null
+  score: number | null
+  total: number | null
+  user_id: string | null
+}
---
+++ src/app/service/attempts/attempts-service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {supabase} from '../../../environments/environment';
+import {Attempts} from '../../models/attempts/attempts';
+import {TablesInsert} from '../../../environments/supabase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttemptsService {
+
+  public attempts: Attempts | null = null
+
+
+ public async insertAttempt(attempt: Attempts) {
+
+    const newAttempt: TablesInsert<'attempts'> = {
+      quiz_id: this.attempts?.quiz_id ?? null,
+      score: this.attempts?.score ?? null,
+      total: this.attempts?.total ?? null,
+      user_id: this.attempts?.user_id ?? null,
+    }
+
+    const { data, error } = await supabase
+      .from('attempts')
+      .insert([
+        newAttempt
+      ])
+      .select()
+    if (error) {
+      console.log("erreur sur l'insertion des attempts", error);
+    }
+
+  }
+}
